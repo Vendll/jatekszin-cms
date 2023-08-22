0f4340a3e461719fbfc94b65f6efa7d0cc1d8e76
Vendll <utto.vendel@icloud.com>
2023-08-22 14:14:55 +0200

    kep

--- src/collections/Munkatars.ts
+++ src/collections/Munkatars.ts
@@ -1,30 +1,37 @@
 import { CollectionConfig } from 'payload/types';
 
 const Munkatars: CollectionConfig = {
-  slug: 'munkatars',
-  labels: {
-    singular: 'Munkatárs',
-    plural: 'Munkatársak',
-    },
-  admin: {
-    useAsTitle: 'name',
-  },
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-        name: 'name',
-        label: 'Név',
-        type: 'text',
-        required: true,
-        },
-    {
-        name: 'szinesz',
-        label: 'Színész',
-        type: 'checkbox',
-    },
-  ],
+	slug: 'munkatars',
+	labels: {
+		singular: 'Munkatárs',
+		plural: 'Munkatársak'
+	},
+	admin: {
+		useAsTitle: 'name'
+	},
+	access: {
+		read: () => true
+	},
+	fields: [
+		{
+			name: 'name',
+			label: 'Név',
+			type: 'text',
+			required: true
+		},
+		{
+			name: 'szinesz',
+			label: 'Színész',
+			type: 'checkbox'
+		},
+		{
+			name: 'thumbnail',
+			label: 'Kép',
+			type: 'upload',
+			relationTo: 'media',
+			required: false
+		}
+	]
 };
 
 export default Munkatars;
