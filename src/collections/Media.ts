29974b7b85bac4dba51727b91777b859f287423a
Vendll <utto.vendel@icloud.com>
2023-08-22 16:04:53 +0200

    cloud storage

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,35 +1,35 @@
-import path from 'path'
-import type { CollectionConfig } from 'payload/types'
+import path from 'path';
+import type { CollectionConfig } from 'payload/types';
 
 export const Media: CollectionConfig = {
-  slug: 'media',
-  upload: {
-    staticDir: path.resolve(__dirname, '../../media'),
-    imageSizes: [
-      { name: 'small', width: 320,formatOptions: { format: 'webp' } },
-      { name: 'medium', width: 640,formatOptions: { format: 'webp' } },
-      { name: 'large', width: 1280, formatOptions: { format: 'webp' } },
-      { name: 'xlarge', width: 1920, formatOptions: { format: 'webp' } },
-    ],  
-    formatOptions: { format: 'webp' }  
-  },
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'caption',
-      type: 'richText',
-      admin: {
-        elements: ['link'],
-      },
-    },
-  ],
-}
+	slug: 'media',
+	upload: {
+		staticDir: path.resolve(__dirname, '../../media'),
+		imageSizes: [
+			{ name: 'small', width: 320, formatOptions: { format: 'webp' } },
+			{ name: 'medium', width: 640, formatOptions: { format: 'webp' } },
+			{ name: 'large', width: 1280, formatOptions: { format: 'webp' } },
+			{ name: 'xlarge', width: 1920, formatOptions: { format: 'webp' } }
+		],
+		formatOptions: { format: 'webp' }
+	},
+	access: {
+		read: () => true
+	},
+	fields: [
+		{
+			name: 'alt',
+			type: 'text',
+			required: true
+		},
+		{
+			name: 'caption',
+			type: 'richText',
+			admin: {
+				elements: ['link']
+			}
+		}
+	]
+};
 
-export default Media
+export default Media;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import Users from './collections/Users';
 import seo from '@payloadcms/plugin-seo';
 import BeforeDashboard from './components/BeforeDashboard';
-import  Media  from './collections/Media';
+import Media from './collections/Media';
 import GaleriaKep from './collections/GaleriaKep';
 import Eloadasok from './collections/Eloadasok';
 import Munkatars from './collections/Munkatars';
@@ -13,81 +13,98 @@ import Musor from './collections/Musor';
 import Kezdolap from './globals/Kezdolap';
 import Menu from './globals/Menu';
 import Hirek from './collections/Hirek';
+import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
 
-
+const s3AdapterConfig = {
+	config: {
+		credentials: {
+			accessKeyId: process.env.S3_ACCESS_KEY_ID,
+			secretAccessKey: process.env.S3_ACCESS_SECRET
+		},
+		endpoint: process.env.S3_ENDPOINT,
+		region: 'unknown',
+		forcePathStyle: process.env.S3_FORCE_PATH_STYLE === 'true'
+	},
+	bucket: process.env.S3_BUCKET
+};
 
 const generateTitle = ({ doc }) => {
-  return `${doc.title.value} - Játékszín`
-}
+	return `${doc.title.value} - Játékszín`;
+};
 
 const generateDescription = ({ doc }) => {
-  return doc.description.value
-}
+	return doc.description.value;
+};
 
 const generateImage = ({ doc }) => {
-  if (doc.hero.value) {
-    return doc.hero.value
-  }
-  return doc.thumbnail.value
-}
-
+	if (doc.hero.value) {
+		return doc.hero.value;
+	}
+	return doc.thumbnail.value;
+};
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    components: {
-      // The BeforeDashboard component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import BeforeDashboard statement on line 5.
-      beforeDashboard: [BeforeDashboard],
-    }
-  },
-  collections: [
-    Users,
-    Eloadasok,
-    Munkatars,
-    Szereplo,
-    Kozremukodok,
-    Musor,
-    Media,
-    GaleriaKep,
-    Hirek,
-  ],
-  globals: [
-    Menu,
-    Kezdolap,
-  ],
-  typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
-  },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
-  },
-  plugins: [
-    seo({
-    collections: ['eloadasok', 'hirek'],
-    globals: ['kezdolap'],
-    generateTitle,
-    generateDescription,
-    generateImage,
-    generateURL: ({ doc }: any) => {
-      console.log(doc);
-      if (doc.fields.author) {
-        console.log("Előadás");
-        return `http://localhost:3000/eloadasok/${doc.fields.slug.value}`
-      }
-      else if (doc.fields.content) {
-        console.log("Hír");
-        return `http://localhost:3000/hirek/${doc.fields.slug.value}`
-      }
-      else if (doc.fields.heroes) {
-        console.log("Kezdőlap");
-        return `http://localhost:3000/`
-      } else {
-        console.log("Nincs ilyen collection");
-      }
-    },
-    uploadsCollection: 'media',
-    tabbedUI: true,
-  }),
- ]
+	admin: {
+		user: Users.slug,
+		components: {
+			// The BeforeDashboard component renders the 'welcome' block that you see after logging into your admin panel.
+			// Feel free to delete this at any time. Simply remove the line below and the import BeforeDashboard statement on line 5.
+			beforeDashboard: [BeforeDashboard]
+		}
+	},
+	collections: [
+		Users,
+		Eloadasok,
+		Munkatars,
+		Szereplo,
+		Kozremukodok,
+		Musor,
+		Media,
+		GaleriaKep,
+		Hirek
+	],
+	globals: [Menu, Kezdolap],
+	typescript: {
+		outputFile: path.resolve(__dirname, 'payload-types.ts')
+	},
+	graphQL: {
+		schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql')
+	},
+	plugins: [
+		seo({
+			collections: ['eloadasok', 'hirek'],
+			globals: ['kezdolap'],
+			generateTitle,
+			generateDescription,
+			generateImage,
+			generateURL: ({ doc }: any) => {
+				console.log(doc);
+				if (doc.fields.author) {
+					console.log('Előadás');
+					return `http://localhost:3000/eloadasok/${doc.fields.slug.value}`;
+				} else if (doc.fields.content) {
+					console.log('Hír');
+					return `http://localhost:3000/hirek/${doc.fields.slug.value}`;
+				} else if (doc.fields.heroes) {
+					console.log('Kezdőlap');
+					return `http://localhost:3000/`;
+				} else {
+					console.log('Nincs ilyen collection');
+				}
+			},
+			uploadsCollection: 'media',
+			tabbedUI: true
+		}),
+		cloudStorage({
+			collections: {
+				media: {
+					adapter: s3Adapter(s3AdapterConfig),
+					generateFileURL: ({ filename }) => {
+						return `${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${filename}`;
+					}
+				}
+			}
+		})
+	]
 });
