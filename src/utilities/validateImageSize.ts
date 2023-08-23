930747773c518f1b4b6c14bcdf0cfb98af1c8542
Vendll <utto.vendel@icloud.com>
2023-08-23 12:28:15 +0200

    -

--- src/utilities/validateImageSize.ts
+++ src/utilities/validateImageSize.ts
@@ -1,14 +1,14 @@
 async function validateImageSize(value) {
-    console.log(value);
-    if (!value) return;
-    const resp = await fetch(`http://localhost:3000/api/media/${value}`)
-    const data = await resp.json();
-    const { width, height } = data
-    if (width > 1920 || height > 1200) {
-        return "A kép mérete túl nagy"
-    } else if (width < 350 || height < 350) {
-        return "A kép mérete túl kicsi"
-    }
+	console.log(value);
+	if (!value) return;
+	const resp = await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/media/${value}`);
+	const data = await resp.json();
+	const { width, height } = data;
+	if (width > 1920 || height > 1200) {
+		return 'A kép mérete túl nagy';
+	} else if (width < 350 || height < 350) {
+		return 'A kép mérete túl kicsi';
+	}
 }
 
 export default validateImageSize;
