45f11b80470ce7c9f476bcc0e440de44026fd23d
Vendll <utto.vendel@icloud.com>
2023-08-23 13:38:40 +0200

    golbal access

--- src/globals/Menu.ts
+++ src/globals/Menu.ts
@@ -1,29 +1,32 @@
-import { GlobalConfig } from "payload/types";
+import { GlobalConfig } from 'payload/types';
 
 const Menu: GlobalConfig = {
-  slug: "menu",
-  label: "Menü",
-  fields: [
-    {
-        name: "navItems",
-        label: "Menüpontok",
-        type: "array",
-        fields: [
-            {
-                name: "title",
-                label: "Cím",
-                type: "text",
-                required: true,
-            },
-            {
-                name: "link",
-                label: "Link",
-                type: "text",
-                required: true,
-            },
-        ],
-    }
-  ]
+	slug: 'menu',
+	label: 'Menü',
+	access: {
+		read: () => true
+	},
+	fields: [
+		{
+			name: 'navItems',
+			label: 'Menüpontok',
+			type: 'array',
+			fields: [
+				{
+					name: 'title',
+					label: 'Cím',
+					type: 'text',
+					required: true
+				},
+				{
+					name: 'link',
+					label: 'Link',
+					type: 'text',
+					required: true
+				}
+			]
+		}
+	]
 };
 
 export default Menu;
