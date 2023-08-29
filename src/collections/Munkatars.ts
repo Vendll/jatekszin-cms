260e6413fbf5540e50e27e1461c0c8160b612f01
Vendll <utto.vendel@icloud.com>
2023-08-29 14:49:57 +0200

    -

--- src/collections/Munkatars.ts
+++ src/collections/Munkatars.ts
@@ -40,6 +40,38 @@ const Munkatars: CollectionConfig = {
 			type: 'checkbox'
 		},
 		{
+			name: 'alkategoria',
+			label: 'Alkategória',
+			type: 'select',
+			options: [
+				{ label: 'Dalszövegírók, zeneszerzők', value: 'dalszovegiro' },
+				{ label: 'Díszlettervezők', value: 'diszlettervezo' },
+				{ label: 'Dramaturgok', value: 'dramaturg' },
+				{ label: 'Jelmeztervezők', value: 'jelmeztervezo' },
+				{ label: 'Karmesterek, zenészek', value: 'karmester' },
+				{ label: 'Koreográfusok', value: 'koreografus' },
+				{ label: 'Rendezők', value: 'rendezo' },
+				{ label: 'Szövegírók, fordítók', value: 'szovegiro' },
+				{ label: 'Világítástervezők', value: 'vilagitastervezo' },
+				{ label: 'Művészeti ügykezelés', value: 'muvugykezeles' },
+				{ label: 'Gazdasági munkatársak', value: 'gazdasagi' },
+				{ label: 'Tájszervezés', value: 'tajszervezes' },
+				{ label: 'Szervezés', value: 'szervezes' },
+				{ label: 'Kommunikáció', value: 'kommunikacio' },
+				{ label: 'Kellékesek', value: 'kellekes' },
+				{ label: 'Műszaki vezetés', value: 'muszakivez' },
+				{ label: 'Díszítők', value: 'diszito' },
+				{ label: 'Fodrászat', value: 'fodraszat' },
+				{ label: 'Öltöztetők', value: 'oltoztetok' },
+				{ label: 'Világosítók', value: 'vilagosito' },
+				{ label: 'Hangosítók', value: 'hangosito' },
+				{ label: 'Büfé', value: 'bufe' },
+				{ label: 'Portások', value: 'portasok' },
+				{ label: 'Nézőtéri munkatársak', value: 'nezoterimunkatars' },
+				{ label: 'Takarító', value: 'takarito' }
+			]
+		},
+		{
 			name: 'titulus',
 			label: 'Titulus',
 			type: 'text',
