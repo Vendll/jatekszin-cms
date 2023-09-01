2ec9b6b7b97e6ef2f65930f869d6d07c26058713
Vendll <utto.vendel@icloud.com>
2023-09-01 09:16:02 +0200

    -

--- src/collections/Szereplo.ts
+++ src/collections/Szereplo.ts
@@ -1,44 +1,51 @@
 import { CollectionConfig } from 'payload/types';
 
 const Szereplo: CollectionConfig = {
-  slug: 'szereplo',
-  labels: {
-    singular: 'Szereplő',
-    plural: 'Szereplők',
-    },
-  admin: {
-    useAsTitle: 'name',
-  },
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-        name: 'szinesz',
-        label: 'Színész',
-        type: 'relationship',
-        relationTo: ['munkatars'],
-        hasMany: false,
-        admin: {
-            allowCreate: true,
-        },
-        filterOptions: ({ relationTo }) => {
-            // returns a Where query dynamically by the type of relationship
-            
-            if (relationTo === "munkatars") {
-              return {
-                szinesz: { equals: true },
-              };
-            }
-          },
-        },
-    {
-        name: 'name',
-        label: 'Név',
-        type: 'text',
-        required: true,
-        },
-  ],
+	slug: 'szereplo',
+	labels: {
+		singular: 'Szereplő',
+		plural: 'Szereplők'
+	},
+	admin: {
+		useAsTitle: 'name'
+	},
+	access: {
+		read: () => true
+	},
+	fields: [
+		{
+			name: 'szinesz',
+			label: 'Színész',
+			type: 'relationship',
+			relationTo: ['munkatars'],
+			hasMany: false,
+			admin: {
+				allowCreate: true
+			},
+			filterOptions: ({ relationTo }) => {
+				// returns a Where query dynamically by the type of relationship
+
+				if (relationTo === 'munkatars') {
+					return {
+						szinesz: { equals: true }
+					};
+				}
+			}
+		},
+		{
+			name: 'name',
+			label: 'Név',
+			type: 'text',
+			required: true
+		},
+		{
+			name: 'eloadasKep',
+			label: 'Előadás specifikus kép',
+			type: 'upload',
+			relationTo: 'media',
+			required: false
+		}
+	]
 };
 
 export default Szereplo;
